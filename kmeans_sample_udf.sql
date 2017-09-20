32d0c99ef278efa006d04cb7eb3380d021d3af10
Atsushi Neki <neki.atsushi@jp.fujitsu.com>
2017-09-20 05:55:48 +0000

    Updated UDF prototype
    - for Testing

--- kmeans_sample_udf.sql
+++ kmeans_sample_udf.sql
@@ -1,8 +1,9 @@
 -- DROP FUNCTION IF EXISTS kmeans_sample_udf( data_array_entry text[] );
 DROP TABLE IF EXISTS km_sample CASCADE;
 DROP TABLE IF EXISTS kmeans_sample_udf_ret CASCADE;
+DROP TABLE IF EXISTS km_sample_result_tbl CASCADE;
 
-CREATE TABLE kmeans_sample_udf_ret( pid integer, cluster_id integer );
+CREATE TABLE kmeans_sample_udf_ret( pid integer, points1 double precision, points2 double precision, points3 double precision, points4 double precision, points5 double precision, points6 double precision, points7 double precision, points8 double precision, points9 double precision, points10 double precision, points11 double precision, points12 double precision, points13 double precision, cluster_id integer );
 
 CREATE OR REPLACE FUNCTION kmeans_sample_udf(
     data_array_entry text[],
@@ -14,7 +15,6 @@ CREATE OR REPLACE FUNCTION kmeans_sample_udf(
 RETURNS setof kmeans_sample_udf_ret AS
 $$
 DECLARE
-    rec kmeans_sample_udf_ret_type;
     i text;
     entry text = '';
 BEGIN
@@ -41,8 +41,9 @@ BEGIN
                             ''madlib.%s'', $2, $3)', fn_dist, agg_centroid )
     USING num_cluster, max_num_iterations, min_frac_reassigned;
 
-    RETURN QUERY SELECT data.pid,  (madlib.closest_column(centroids, points)).column_id as cluster_id
-    FROM km_sample_src as data, km_result
+    RETURN QUERY SELECT data.pid, data1.points1, data1.points2, data1.points3, data1.points4, data1.points5, data1.points6, data1.points7, data1.points8, data1.points9, data1.points10, data1.points11, data1.points12, data1.points13, (madlib.closest_column(centroids, points)).column_id as cluster_id
+    FROM km_sample_src as data, km_sample as data1, km_result
+    WHERE data.pid = data1.pid
     ORDER BY data.pid;
     
     RETURN;
@@ -63,21 +64,23 @@ CREATE TABLE km_sample(pid int,
     points10 double precision,
     points11 double precision,
     points12 double precision,
-    points13 double precision
+    points13 double precision,
+    cluster_id integer
 );
 
 INSERT INTO km_sample VALUES
-(1,  14.23, 1.71, 2.43, 15.6, 127, 2.8, 3.0600, 0.2800, 2.29, 5.64, 1.04, 3.92, 1065),
-(2,  13.2, 1.78, 2.14, 11.2, 1, 2.65, 2.76, 0.26, 1.28, 4.38, 1.05, 3.49, 1050),
-(3,  13.16, 2.36,  2.67, 18.6, 101, 2.8,  3.24, 0.3, 2.81, 5.6799, 1.03, 3.17, 1185),
-(4,  14.37, 1.95, 2.5, 16.8, 113, 3.85, 3.49, 0.24, 2.18, 7.8, 0.86, 3.45, 1480),
-(5,  13.24, 2.59, 2.87, 21, 118, 2.8, 2.69, 0.39, 1.82, 4.32, 1.04, 2.93, 735),
-(6,  14.2, 1.76, 2.45, 15.2, 112, 3.27, 3.39, 0.34, 1.97, 6.75, 1.05, 2.85, 1450),
-(7,  14.39, 1.87, 2.45, 14.6, 96, 2.5, 2.52, 0.3, 1.98, 5.25, 1.02, 3.58, 1290),
-(8,  14.06, 2.15, 2.61, 17.6, 121, 2.6, 2.51, 0.31, 1.25, 5.05, 1.06, 3.58, 1295),
-(9,  14.83, 1.64, 2.17, 14, 97, 2.8, 2.98, 0.29, 1.98, 5.2, 1.08, 2.85, 1045),
-(10, 13.86, 1.35, 2.27, 16, 98, 2.98, 3.15, 0.22, 1.8500, 7.2199, 1.01, 3.55, 1045);
+(1,  14.23, 1.71, 2.43, 15.6, 127, 2.8, 3.0600, 0.2800, 2.29, 5.64, 1.04, 3.92, 1065, 0),
+(2,  13.2, 1.78, 2.14, 11.2, 1, 2.65, 2.76, 0.26, 1.28, 4.38, 1.05, 3.49, 1050, 0),
+(3,  13.16, 2.36,  2.67, 18.6, 101, 2.8,  3.24, 0.3, 2.81, 5.6799, 1.03, 3.17, 1185, 0),
+(4,  14.37, 1.95, 2.5, 16.8, 113, 3.85, 3.49, 0.24, 2.18, 7.8, 0.86, 3.45, 1480, 0),
+(5,  13.24, 2.59, 2.87, 21, 118, 2.8, 2.69, 0.39, 1.82, 4.32, 1.04, 2.93, 735, 0),
+(6,  14.2, 1.76, 2.45, 15.2, 112, 3.27, 3.39, 0.34, 1.97, 6.75, 1.05, 2.85, 1450, 0),
+(7,  14.39, 1.87, 2.45, 14.6, 96, 2.5, 2.52, 0.3, 1.98, 5.25, 1.02, 3.58, 1290, 0),
+(8,  14.06, 2.15, 2.61, 17.6, 121, 2.6, 2.51, 0.31, 1.25, 5.05, 1.06, 3.58, 1295, 0),
+(9,  14.83, 1.64, 2.17, 14, 97, 2.8, 2.98, 0.29, 1.98, 5.2, 1.08, 2.85, 1045, 0),
+(10, 13.86, 1.35, 2.27, 16, 98, 2.98, 3.15, 0.22, 1.8500, 7.2199, 1.01, 3.55, 1045, 0);
 
+create table km_sample_result_tbl as
 select * from kmeans_sample_udf( ARRAY[
         'points1',
         'points2',
